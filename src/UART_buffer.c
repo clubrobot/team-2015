652980c763ccc4c07864f8a09364090370e64351
gaterz <julien.maurat@insa-rennes.fr>
2015-04-13 18:58:12 +0200

    + Base files for UART buffers : UART_buffer.c	UART_buffer.h
    Signed-off-by: gaterz <julien.maurat@insa-rennes.fr>

---
+++ src/UART_buffer.c
@@ -0,0 +1,8 @@
+/*
+ * UART_buffer.c
+ *
+ *  Created on: Apr 13, 2015
+ *      Author: julien
+ */
+
+
---
+++ src/UART_buffer.h
@@ -0,0 +1,13 @@
+/*
+ * UART_buffer.h
+ *
+ *  Created on: Apr 13, 2015
+ *      Author: julien
+ */
+
+#ifndef UART_BUFFER_H_
+#define UART_BUFFER_H_
+
+
+
+#endif /* UART_BUFFER_H_ */
