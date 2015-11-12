81e623ebc7889f6a4863fb6ad0df2771b7e1f7e6
Ulysse Darmet <darmet.ulysse@gmail.com>
2015-11-12 17:48:55 +0100

    Message Refactoring

--- Message.h
+++ Message.h
@@ -24,7 +24,11 @@ protected:
 	uint8_t mdestination; // The receiver's id.
 	uint32_t mdlc; // The length of the internal data.
 	uint8_t* mdata; // The internal data.
+<<<<<<< Upstream, based on branch 'master' of https://github.com/IR-RC/arietta-robot.git
 	uint8_t mcursor; // The position in the data where to retrieve the current variable. See append and retrieve methods.
+=======
+	mutable uint8_t mcursor; // The position in the data where to retrieve the current variable. See append and retrieve methods.
+>>>>>>> 4ac70c5 Message Refactoring
 
 public:
 
@@ -38,12 +42,17 @@ public:
 	virtual ~Message();
 
 	// Get the message's emitter.
+<<<<<<< Upstream, based on branch 'master' of https://github.com/IR-RC/arietta-robot.git
 	uint8_t getEmitter();
+=======
+	uint8_t getEmitter() const;
+>>>>>>> 4ac70c5 Message Refactoring
 
 	// Set the message's emitter.
 	void setEmitter( uint8_t emitter );
 
 	// Get the message's receiver.
+<<<<<<< Upstream, based on branch 'master' of https://github.com/IR-RC/arietta-robot.git
 	uint8_t getReceiver();
 
 	// Set the receiver.
@@ -69,6 +78,33 @@ public:
 
 	// Get the message's raw data (see above).
 	void getRawData( uint8_t* dst );
+=======
+	uint8_t getReceiver() const;
+
+	// Set the receiver.
+	void setReceiver( uint8_t destination );
+
+	// Clear the message's data. This can't be undone.
+	void clearData();
+
+	// Get the length of the message's data.
+	uint32_t getDataLength() const;
+
+	// Copy the message's data to another memory location, assuming it has previously been allocated (using getDataLength).
+	void getData( uint8_t* dst ) const;
+
+	// Set the message's data by erasing the previous one.
+	void setData( const uint8_t data[], uint32_t dlc );
+
+	// Add some new data to the message's one.
+	void appendData( const uint8_t data[], uint32_t dlc );
+
+	// Get the length of the message's entire data. This includes metadata like the message's emitter or receiver.
+	uint32_t getRawDataLength() const;
+
+	// Get the message's raw data (see above).
+	void getRawData( uint8_t* dst ) const;
+>>>>>>> 4ac70c5 Message Refactoring
 
 	// Add a new formatted variable to the message. For example :
 	//
@@ -94,7 +130,11 @@ public:
 	// c = m.retrieve< float >();
 
 	template< typename T >
+<<<<<<< Upstream, based on branch 'master' of https://github.com/IR-RC/arietta-robot.git
 	T retrieve( void )
+=======
+	T retrieve( void ) const
+>>>>>>> 4ac70c5 Message Refactoring
 	{
 		T var;
 		memcpy( &var, mdata + mcursor, sizeof( T ) );
