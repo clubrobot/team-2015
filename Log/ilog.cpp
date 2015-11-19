a36cd836ba4b2457589167e471ebb2b9eda9843e
Ethel Marquer <ethel.marquer@gmail.com>
2015-11-19 15:36:25 +0100

    implementation of timestamp()

--- Log/ilog.cpp
+++ Log/ilog.cpp
@@ -16,3 +16,16 @@ ilog::~ilog() {
 	// TODO Auto-generated destructor stub
 }
 
+std::string ilog::timestamp(){
+
+	std::ostringstream stream;
+	time_t rawtime;
+	tm * timeinfo;
+
+	time(&rawtime);
+	timeinfo = localtime( &rawtime );
+
+	stream << timeinfo->tm_hour <<"h "<<timeinfo->tm_min<<"mn "<<timeinfo->tm_sec <<"s";
+	// The str() function of output stringstreams return a std::string
+	return stream.str();
+}
--- Log/ilog.h
+++ Log/ilog.h
@@ -9,11 +9,14 @@
 #define LOG_ILOG_H_
 
 #include <istream>
+#include <time.h>
+#include <sstream>
 
 class ilog: public std::istream {
 public:
 	ilog();
 	virtual ~ilog();
+	std::string timestamp();
 };
 
 #endif /* LOG_ILOG_H_ */
