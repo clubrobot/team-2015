496a77191ffa5da0c0ec0f47973a2287d2a190f9
HUANGManutea <huang.manutea@gmail.com>
2015-11-24 23:30:34 +0100

    USBCOMServer methods

--- USBCOMServer.cpp
+++ USBCOMServer.cpp
@@ -4,14 +4,36 @@
  *  Created on: Nov 19, 2015
  *      Author: gabriel
  */
-
+#include <fstream>
+#include <unistd.h>
 #include "USBCOMServer.h"
 
-USBCOMServer::USBCOMServer() {
+USBCOMServer::USBCOMServer(string ui, string num, string desc) {
+	//print in the file referenced by Server.ptrfile
+	fstream outputFile ;
+	outputFile.open(USBCOMServer::PTRFILE.c_str(),fstream::out|fstream::app);
+	outputFile << ui+" "+num+" "+desc+"\n";
+	cout << ui+" "+num+" "+desc <<endl;
+	outputFile.close();
+
+	//create the object
+	uuid  = string (ui);
+	numero = string (num);
+	description = string (desc);
 }
 
 USBCOMServer::~USBCOMServer() {
 }
 
 void USBCOMServer::launch(const std::string& UUID) {
+	//UARTServer.launch(UUID);
+}
+
+string USBCOMServer::getTTY()
+{
+	char buf[16];
+	string path = string (USBCOMServer::UUIDFOLDER+uuid);
+	ssize_t len = readlink(path.c_str(),buf,sizeof(buf)-1);
+	buf[len] = '\0';
+	return string(buf);
 }
--- USBCOMServer.h
+++ USBCOMServer.h
@@ -8,12 +8,25 @@
 #ifndef USBCOMSERVER_H_
 #define USBCOMSERVER_H_
 
+#include <string>
 #include <arietta-comlib/Serial/UARTServer.h>
+using namespace std;
 
 class USBCOMServer: public UARTServer {
+private:
+	string uuid;
+	string numero;
+	string description;
+
 public:
-	USBCOMServer();
+	//file to print the config
+	static const  string PTRFILE;
+	//folder containing all the uuid
+	static const string UUIDFOLDER;
+
+	USBCOMServer(string ui, string num, string desc);
 	virtual ~USBCOMServer();
+	string getTTY();
 	void launch(const std::string& UUID);
 };
 
