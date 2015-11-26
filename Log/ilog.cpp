d81ea79a184a9d2b5af6df9fb8b14ec0da12cfc9
Ethel Marquer <ethel.marquer@gmail.com>
2015-11-26 19:35:34 +0100

    add functions for the log client. +readFile and +getlenght
    (errors)

--- Log/ilog.cpp
+++ Log/ilog.cpp
@@ -16,6 +16,28 @@ ilog::~ilog() {
 	// TODO Auto-generated destructor stub
 }
 
-void ilog::readFile(const char* filename) {
-	// TODO implement this method
+void ilog::readFile(const char* filename,char delemiter){
+	std::ifstream file(filename);
+	std::istream_iterator<char> it(file);
+
+	const int n = 60;
+	char *buffer = new char[n];
+
+	file.seekg(ios_base::beg);
+	file.get(buffer, n, delemiter);
+
+	while (!file.eof()) {
+		std::cout << buffer << std::endl;
+		it++;
+		file.get(buffer, n, delemiter);
+	}
+	file.close();
+}
+
+int ilog::getlenght(const char* filename) {
+	std::ifstream ifile(filename);
+	int length;
+	ifile.seekg(0,ios_base::end);
+	length = ifile.tellg();
+	return length;
 }
--- Log/ilog.h
+++ Log/ilog.h
@@ -11,12 +11,15 @@
 #include <istream>
 #include <time.h>
 #include <sstream>
+#include <iostream>
+#include <iterator>
 
 class ilog: public std::istream{
 public:
 	ilog();
 	virtual ~ilog();
-	void readFile(const char* filename);
+	void readFile(const char* filename,char delemiter);
+	int getlenght(const char* filename);
 };
 
 #endif /* LOG_ILOG_H_ */
