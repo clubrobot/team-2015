d115c95fb0ace7c2e472071be57d8171301d6c42
Ulysse Darmet <darmet.ulysse@gmail.com>
2015-11-30 17:44:08 +0100

    updated main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,23 +4,45 @@
  *  Created on: 1 oct. 2015
  *      Author: ethel
  */
+
+#include <iostream>
 #include "../Message.h"
-#include "../MessageBuilder.h"
-#include "../Parser.h"
+
+using namespace std;
 
 int main(int argc, char **argv) {
-	int nb1 =12;
-	int nb2 =5;
-	float fl1 = 3.2;
-	Parser parser;
-	Message msg;
-	parser.open(&msg);
-	//	parser.putData(data,12);//Ne fonctionne pas
-	parser.putInt(nb1);
-	parser.putInt(nb2);
-	parser.putFloat(fl1);
-	parser.commit();
 
+	// BUILD MESSAGE
+
+	int inb1	= 12; // 'i' stands for 'input'
+	int inb2	= 5;
+	float ifl1	= 3.2;
+
+	Message imsg;
+	imsg.append< int >( inb1 );
+	imsg.append( inb2 ); // Also works if omitting the template parameter
+	imsg.append( ifl1 ); // Automatically detect the type of the variable
+
+	// COMPUTE DATA
+
+	size_t length = imsg.getRawDataLength();
+	uint8_t* data = new uint8_t[ length ];
+	imsg.getRawData( data );
+
+	// READ MESSAGE
+
+	Message omsg( data, length ); // 'o' stands for 'output'
+	int onb1 = omsg.retrieve< int >(); // The type must be passed here
+	int onb2 = omsg.retrieve< int >();
+	float ofl1 = omsg.retrieve< float >();
+
+	// CHECK
+
+	cout << inb1 << " =? " << onb1 << endl;
+	cout << inb2 << " =? " << onb2 << endl;
+	cout << ifl1 << " =? " << ofl1 << endl;
+
+	delete[] data;
 	return 0;
 }
 
