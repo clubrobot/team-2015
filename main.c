f9cef1c39a5a12cf2d0355130f10fa09aeb755ff
Ulysse Darmet <darmet.ulysse@gmail.com>
2015-12-15 21:49:57 +0100

    Refactored main

--- main.c
+++ main.c
@@ -8,119 +8,233 @@
 #define USB_PATH FOLDER_PATH "usbmapping.cfg"
 #define TCP_PATH FOLDER_PATH "TCP.cfg"
 
-void showHelp()
-{
-	printf("Usage:\n");
-	printf("1) robot usb list ---pour lister les uuid\n");
-	printf("2) robot usb clear ---pour effacer tous les uuid\n");
-	printf("3) robot usb add id [description] ---pour ajouter un uuid\n");
-	printf("4) robot usb remove id ---pour supprimer un uuid en particulier\n");
-	printf("5) robot tcp show ---affiche l'ip et le port\n");
-	printf("6) robot tcp set ip port ---attribue une nouvelle ip et un port\n");
-}
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#define LOAD_USB_MAPPING( map )\
+	{ initUSBMapping( &map ); if( !loadUSBMapping( &map, USB_PATH ) )\
+	{ printf( "Can't load USB mapping: %s", USB_PATH ); return -1; } }
+
+#define SAVE_USB_MAPPING( map )\
+	{ if( !saveUSBMapping( &map, USB_PATH ) )\
+	{ printf( "Can't save USB mapping" ); return -1; } }
+
+#define LOAD_TCP_CONFIG( cfg )\
+	{ initTCPConfig( &cfg ); if( !loadTCPConfig( &cfg, TCP_PATH ) )\
+	{ printf( "Can't load TCP config: %s", TCP_PATH ); return -1; } }
+
+#define SAVE_TCP_CONFIG( cfg )\
+	{ if( !saveTCPConfig( &cfg, TCP_PATH ) )\
+	{ printf( "Can't save TCP config" ); return -1; } }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// USB mapping related commands
+int usb_list( int argc, char* argv[] );
+int usb_add( int argc, char* argv[] );
+int usb_remove( int argc, char* argv[] );
+int usb_clear( int argc, char* argv[] );
+
+// TCP config related commands
+int tcp_show( int argc, char* argv[] );
+int tcp_set( int argc, char* argv[] );
+
+// Miscellaneous commands
+int help( int argc, char* argv[] );
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 int main( int argc, char* argv[] )
 {
-    USBMapping map;
-    TCPConfig config;
-    int help = 0;
+	if( !createDirectory( FOLDER_PATH ) )
+	{
+		printf( "Can't create directory: %s", FOLDER_PATH );
+		return -1;
+	}
+	if( argc > 1 )
+	{
+		if( strcmp( argv[ 1 ], "usb" ) == 0 )
+		{
+			if( argc > 2 )
+			{
+				if( strcmp( argv[ 2 ], "list" ) == 0 )
+				{
+					return usb_list( argc, argv );
+				}
+				else if( strcmp( argv[ 2 ], "add" ) == 0 )
+				{
+					return usb_add( argc, argv );
+				}
+				else if( strcmp( argv[ 2 ], "remove" ) == 0 )
+				{
+					return usb_remove( argc, argv );
+				}
+				else if( strcmp( argv[ 2 ], "clear" ) == 0 )
+				{
+					return usb_clear( argc, argv );
+				}
+			}
+		}
+		else if( strcmp( argv[ 1 ], "tcp" ) == 0 )
+		{
+			if( argc > 2 )
+			{
+				if( strcmp( argv[ 2 ], "show" ) == 0 )
+				{
+					return tcp_show( argc, argv );
+				}
+				else if( strcmp( argv[ 2 ], "set" ) == 0 )
+				{
+					return tcp_set( argc, argv );
+				}
+			}
+		}
+	}
+	return help( argc, argv );
+}
 
-    createDirectory( FOLDER_PATH );
+/////////////////////////////////////////////////////////////////////////////////////////
 
-    initUSBMapping( &map );
-    loadUSBMapping( &map, USB_PATH );
-    if(argc > 1)
-    {
-    	if(strcmp(argv[1],"usb")== 0)
-    	{
-    		if(argc > 2)
-    		{
-    		if(strcmp(argv[2],"list")== 0)
-    		{
-    					if(map.numSlots != 0) printUSBMapping(&map);
-    					else printf("%s est vide !\n",USB_PATH);
-    		}else
-    		if(strcmp(argv[2],"add")== 0)
-    		{
-    			char *desc = NULL;
-    			int id = -1, i;
-    			char uuid[ 20 ];
-    			UUIDWatcher w;
-    			initUUIDWatcher( &w );
-    			if(argc > 3)
-    			{
-    			 sscanf(argv[3],"%d",&id);
-    			 if(argc > 4 ){ desc = argv[4];
-    				 for(i=5;i < argc; ++i){ strcat( desc," "); strcat(desc,argv[i]);}}
-    			 waitUUID( &w, uuid );
-    			 if(id > -1 ) addUSBSlot(&map, uuid, id, desc );
-    			 closeUUIDWatcher( &w );
-    			 saveUSBMapping(&map,USB_PATH);
-    			}else help =1;
-    		}
-    		else
-    		if(strcmp(argv[2],"clear")== 0)
-    		{
-    					initUSBMapping( &map );
-    					saveUSBMapping(&map,USB_PATH);
-    		}
-    		else
-    		if(strcmp(argv[2],"remove")== 0)
-    		{
-    			int i = -1, j=0, v = 0;
-    				if(argc > 3) sscanf(argv[3],"%d",&i);printf("-- %d --\n",i);
-    					if(i > -1 )
-    					{
-    						for(j=0;j<map.numSlots;++j)
-    						{
-    							if(map.slots[j].id == i)
-    							{
-    							 removeUSBSlot(&map,j);
-    							 v = 1;
-    							}
-    						}
-    						if(v == 1)
-    						saveUSBMapping(&map,USB_PATH);
-    						else
-    							printf("l'id est incorrect !\n");
-    					}
-    					else
-    					{
-    						printf("le paramettre id est absent !\n");
-    						printf("usage:\n robot usb remove id\n");
-    					}
-    		}else help = 1;
-    	 }else help =1;
-    	}
-    	else
-    	{
+int usb_list( int argc, char* argv[] )
+{
+	USBMapping map;
+	LOAD_USB_MAPPING( map )
+	if( map.numSlots != 0 )
+	{
+		printUSBMapping( &map );
+		return 0;
+	}
+	else
+	{
+		printf( "USB mapping is empty\n" );
+		return 0;
+	}
+}
 
-    		if(strcmp(argv[1],"tcp")== 0)
-    		{
+/////////////////////////////////////////////////////////////////////////////////////////
 
-    			initTCPConfig(&config);
-    			loadTCPConfig(&config,TCP_PATH);
-                if(argc < 3){ showHelp(); return 0;}
-    			if(strcmp(argv[2],"show")== 0)
-    			{
-    				printTCPConfig(&config);
-    			}else
-    			if(strcmp(argv[2],"set")== 0)
-    			{
-    				if(argc > 4)
-    				{
-						int i = -1;
-						sscanf(argv[4],"%d",&i);
-						setTCPConfig(&config, argv[3], i);
-						saveTCPConfig( &config, TCP_PATH );
-    				}else help =1;
-    			}else help =1;
-    		} else help =1;
-    	}
-    }
-    else help =1;
+int usb_add( int argc, char* argv[] )
+{
+	USBMapping map;
+	int id = -1;
+	char desc[ 141 ];
+	char uuid[ 20 ];
+	UUIDWatcher w;
+	int i;
+	LOAD_USB_MAPPING( map )
+	if( argc > 3 )
+	{
+		sscanf( argv[ 3 ], "%d", &id );
+		if( argc > 4 )
+		{
+			strcpy( desc, argv[ 4 ] );
+			for( i = 5; i < argc; i++ )
+			{
+				strcat( desc, " " );
+				strcat( desc, argv[ i ] );
+			}
+		}
+		initUUIDWatcher( &w );
+		scanUUID( &w, uuid );
+		closeUUIDWatcher( &w );
+		if( id > -1 )
+		{
+			addUSBSlot(&map, uuid, id, desc );
+			SAVE_USB_MAPPING( map )
+			return 0;
+		}
+		else
+		{
+			printf( "ID %d is invalid\n", id );
+			return -1;
+		}
+	}
+	return help( argc, argv );
+}
 
-    if(help) showHelp();
+/////////////////////////////////////////////////////////////////////////////////////////
 
-    return 0;
+int usb_remove( int argc, char* argv[] )
+{
+	USBMapping map;
+	int found;
+	int i, j;
+	LOAD_USB_MAPPING( map )
+	if( argc > 3 )
+	{
+		i = -1;
+		sscanf( argv[ 3 ], "%d", &i );
+		if( i > -1 )
+		{
+			for( j = 0; j < map.numSlots; j++ )
+			{
+				if( map.slots[ j ].id == i )
+				{
+					removeUSBSlot( &map, j );
+					found = 1;
+				}
+			}
+			if( found )
+			{
+				SAVE_USB_MAPPING( map )
+				return 0;
+			}
+		}
+		else
+		{
+			printf( "ID %d not found\n", i );
+			return -1;
+		}
+	}
+	return help( argc, argv );
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+int usb_clear( int argc, char* argv[] )
+{
+	USBMapping map;
+	initUSBMapping( &map );
+	SAVE_USB_MAPPING( map )
+	return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+int tcp_show( int argc, char* argv[] )
+{
+	TCPConfig cfg;
+	LOAD_TCP_CONFIG( cfg )
+	printTCPConfig( &cfg );
+	return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+int tcp_set( int argc, char* argv[] )
+{
+	TCPConfig cfg;
+	int i = -1;
+	LOAD_TCP_CONFIG( cfg )
+	if( argc > 4 )
+	{
+		sscanf( argv[ 4 ], "%d" , &i );
+		setTCPConfig( &cfg, argv[ 3 ], i );
+		SAVE_TCP_CONFIG( cfg )
+		return 0;
+	}
+	return help( argc, argv );
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+int help( int argc, char* argv[] )
+{
+	printf("Usage:\n");
+	printf("\trobot usb list\n" );
+	printf("\trobot usb clear\n" );
+	printf("\trobot usb add id \"description\"\n");
+	printf("\trobot usb remove id\n");
+	printf("\trobot tcp show\n");
+	printf("\trobot tcp set ip port\n");
+	return -1;
 }
