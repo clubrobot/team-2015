37ca30832b8cb3ed8ab3ce1ff6aa1f83123c0ea4
Ethel Marquer <ethel.marquer@gmail.com>
2016-03-09 10:44:09 +0100

    Implementation of signal handling.

--- main.cpp
+++ main.cpp
@@ -15,8 +15,22 @@ void myterminate() {
 	abort();
 }
 
+/* Signal handler. */
+void hdl(int sig)
+{
+	std::cout << "Server closed" << std::endl;
+	srv.close();
+}
+
+void myterminate2(){
+	signal(SIGTERM, hdl);
+	signal(SIGINT, hdl);
+}
+
+
 int main(){
 	std::set_terminate(myterminate);
+	std::set_terminate(myterminate2);//terminate Ctrl+C and kill sig
 	srv.launch();
 	return 0;
 }
