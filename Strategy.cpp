bbad0d606b55312741c496826aa3baf821a79d11
Ethel Marquer <ethel.marquer@gmail.com>
2016-05-07 00:26:08 +0200

    solved

--- Strategy.cpp
+++ Strategy.cpp
@@ -1,42 +1,60 @@
 #include "Strategy.h"
 
 Strategy::Strategy()
-    : multrason(1, mbrain)
+: mparasol(1, mbrain), multrason(2, mbrain), mmotor(3, mbrain)
 {
-    Startup::initHardware();
-    initAllModules();
-    initAllActions();
+	Startup::initHardware();
+	initAllModules();
+	initAllActions();
 }
 
 Strategy::~Strategy()
 {
-    Startup::exitHardware();
+	Startup::exitHardware();
 }
 
 void Strategy::launch()
 {
-    mbrain.launchModules();
-    initStrategy();
-    mhandler.run();
+	mbrain.launchModules();
+	initStrategy();
+	mhandler.run();
 }
 
 void Strategy::initAllModules()
 {
     // Add all modules to the brain
     mbrain.addModule(&multrason);
+    mbrain.addModule(&mparasol);
+
 }
 
 void Strategy::initAllActions()
 {
-    // Add event listeners
+	// Add event listeners
+
+	mhandler.addEventListener("chrono90" ,[this](EventParams params) {
+		this->mparasol.openParasol();
+	});
+
+	mhandler.addEventListener("chronoFin", [this](EventParams params) {
+		this->mparasol.closeParasol();
+	});
+
+	mhandler.addEventListener("data", [this](EventParams params){
+		uint8_t id, length;
+		params >> id >> length;
+		if(length < 3){
+			this->mmotor.stop();
+		}
+	});
+
 }
 
 void Strategy::initStrategy()
 {
-    Startup::readyAction();
-
     while(!Startup::startCondition());
     std::cout << "Open parasol" << std::endl;
-    multrason.openParasol();
+    mparasol.openParasol();
+
 }
 
